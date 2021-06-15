d73095d896ea170958e80f0e9d98f9954d93d67c
Nastya Domnich <https://github.com/nastyadomnich0099/project.git>
2021-06-15 12:58:18 +0300

    one

--- test/specs/watches.js
+++ test/specs/watches.js
@@ -1,42 +1,42 @@
-// import LoginPage from  '../pageobjects/login.page';
-// import SecurePage from '../pageobjects/secure.page';
+import LoginPage from  '../pageobjects/login.page';
+import SecurePage from '../pageobjects/secure.page';
 
 
 // describe('My Login application', () => {
-// //     it('should login with valid credentials', async () => {
-// //         await LoginPage.open();
+//     it('should login with valid credentials', async () => {
+//         await LoginPage.open();
 
-// //         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-// //         await expect(SecurePage.flashAlert).toBeExisting();
-// //         await expect(SecurePage.flashAlert).toHaveTextContaining(
-// //             'You logged into a secure area!');
-// //     });
-// // });
-
-
-
-// describe('Ebay Product Search', ()=>{
-//     it('should open the main url and verify the title',async () =>{
-//           browser.url('https://www.ebay.com/');
-//           expect(browser).toHaveTitle('Electronics, Cars, Fashion, Collectibles & More | eBay'
-//           );
+//         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+//         await expect(SecurePage.flashAlert).toBeExisting();
+//         await expect(SecurePage.flashAlert).toHaveTextContaining(
+//             'You logged into a secure area!');
 //     });
+// });
 
 
-//     it('should search for a product and verify the search text value',async () =>{
+
+describe('Ebay Product Search', ()=>{
+    it('should open the main url and verify the title',async () =>{
+          browser.url('https://www.ebay.com/');
+          expect(browser).toHaveTitle('Electronics, Cars, Fashion, Collectibles & More | eBay'
+          );
+    });
+
+
+    it('should search for a product and verify the search text value',async () =>{
     
-//      LoginPage.input('laptop');
-//      expect(LoginPage.searchInput).toHaveValue('laptop');
+     LoginPage.input('laptop');
+     expect(LoginPage.searchInput).toHaveValue('laptop');
       
-//     });
+    });
 
-// it('should redirect to a new page and verify the title', () =>{
-//     expect(browser).toHaveTitle('laptop | eBay');
-// });
+it('should redirect to a new page and verify the title', () =>{
+    expect(browser).toHaveTitle('laptop | eBay');
+});
 
-// it('should update the search category', ()=>{
-//     const category = $('#gh-cat option:nth-child(1)');
-//     expect(category).toHaveText('PC Laptops & Netbooks');
-// })
+it('should update the search category', ()=>{
+    const category = $('#gh-cat option:nth-child(1)');
+    expect(category).toHaveText('PC Laptops & Netbooks');
+})
 
-// });
+});
